7a6fcd05b6c778635c528341fc332c88f6c1056e
Brian French <french30@email.franklin.edu>
2025-09-21 15:24:29 -0400

    update landing page

--- ManeEvent/src/App.jsx
+++ ManeEvent/src/App.jsx
@@ -1,21 +1,23 @@
 
 import './App.css'
+import React from 'react'
+import logo from './assets/lightLogo_800X800.png'
 
 
 function App() {
   return (
     <>
-      <div>
-      </div>
-      <h1>Welcome to the Mane Event</h1>
-      <div className="card">
+      <div className="card" style={{ width: '300px', height: '300px', overflow: 'hidden', borderRadius: '10px', boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)', position: 'relative' }}>
+        <img src={logo} style={{ width: '75%', height: '75%', objectFit: 'scale-down' }} />      
+      <div className = "buttons">
         <button>
           Login
         </button>
         <button>
           Sign Up
-        </button>
-      
+          </button>
+         
+      </div>
       </div>
 
       </>
