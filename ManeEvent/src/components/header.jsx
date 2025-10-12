c6b36bb90600da94499747251a7247e270a8c26c
lhans21 <mldkiing@gmail.com>
2025-10-12 16:48:42 -0400

    Update header

--- ManeEvent/src/components/header.jsx
+++ ManeEvent/src/components/header.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "../App.css";
 import logo from "../assets/light logo.png";
+import { useNavigate } from "react-router-dom";
 <assets></assets>
 
 function Header() {
@@ -9,16 +10,20 @@ function Header() {
       <div className="navbar">
         <img
           src={logo}
-          style={{ width: "5%", height: "auto", objectFit: "scale-down", }}
+          onClick={() => window.location.href = '/'}
+          style={{ width: "5%", height: "auto", objectFit: "scale-down", cursor: "pointer" }}
           alt="logo"
         />
+          
         <div className="nav-buttons">
-          <a href="">Home</a>
-          <a href="">About</a>
-          <a href="">FAQ</a>
+          <a href="/">Home</a>
+          <a href="/about">About</a>
+          <a href="/faq">FAQ</a>
         </div>
         <div>
-          <button className="primary">Login</button>
+          <button className="primary" onClick={() => navigate("/login")}>
+            Login
+          </button>
         </div>
       </div>
     </>
