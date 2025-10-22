e035d81b62f22ea346f1f635432f6d0e71271836
Brian French <french30@email.franklin.edu>
2025-10-21 20:20:44 -0400

    --added test document at bottom

--- ManeEvent/src/main.jsx
+++ ManeEvent/src/main.jsx
@@ -3,10 +3,12 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import AppRouter from "./AppRouter.jsx";
 import { UserProvider } from "./components/context/Authorization.jsx";
+import Pop_up from "./testDoc/pop_up.jsx";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <UserProvider>
       <AppRouter />
     </UserProvider>
+    <Pop_up />
   </StrictMode>
 );
--- ManeEvent/src/testDoc/pop_up.jsx
+++ ManeEvent/src/testDoc/pop_up.jsx
@@ -1,27 +1,40 @@
 import React from "react";
 import TestLog from "./testlog";
-
-import TestGetLogs from "./testGetLogs";
+//import TestGetLogs from "./testGetLogs";
 import TestFilter from "./testFilter";
 
 //takes in parmaters used for showing and hinting the pop up, can also pass in children elements if desired
 //use {pros.childeren} to access the children elements
 //can use factory method if needed to customize the pop up more in the future
-function Pop_up(props) {
-  return props.trigger ? (
-    <div className="popup">
+function Pop_up() {
+  return (
+    <div
+      className="bg-[#f054b0]"
+      style={{
+        textAlign: "center",
+        padding: "10px",
+        color: "#2B6150",
+        bottom: "0",
+      }}
+    >
       <hr />
-      <button onClick={() => props.setTrigger(false)}>close</button>
+
       <div className="popup_inner">
-        <h3>Document test</h3> <TestLog />
-        <TestGetLogs />
-        <TestFilter />
+        <h3>FOR DEV ONLY</h3>
+        <div className="flex justify-center gap-3 mt-3">
+          <div className="w-1/2">
+            <TestLog />
+          </div>
+          <div className="w-1/2">
+            <TestFilter />
+          </div>
+        </div>
       </div>
+
       <hr />
     </div>
-  ) : (
-    ""
   );
 }
-//exports the pop up function so it can be used in other files
+
+// Export the pop-up function so it can be used in other files
 export default Pop_up;
