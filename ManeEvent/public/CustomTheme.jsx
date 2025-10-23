6215c68fa9c13dec6c0470defe8952bd879cc37a
Brian French <french30@email.franklin.edu>
2025-10-23 07:16:24 -0400

    update yml

---
+++ ManeEvent/public/CustomTheme.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function CustomTheme() {
+  return (
+    <div className="card">
+      <div className="warningBox">
+        <p>There was an Error</p>
+      </div>
+    </div>
+  );
+}
+
+export default CustomTheme;
--- ManeEvent/tailwind.config.js
+++ ManeEvent/tailwind.config.js
@@ -5,8 +5,11 @@ export default {
     theme: {
       colors: {
         primary: "#2b6150",
+        secondary: "#4EB192",
+        warning: "#f054b0",
         white: "#ffffff",
         black: "#000000",
+
         transparent: "transparent",
         current: "currentColor",
       },
