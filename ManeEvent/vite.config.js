44b7cf22cdec9eeb4529b7be22d40deccb21607e
Brian French <french30@email.franklin.edu>
2025-10-25 12:48:48 -0400

    hardcoded supabasekeys

--- ManeEvent/vite.config.js
+++ ManeEvent/vite.config.js
@@ -3,8 +3,9 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 // This reads the environment variables at build time from process.env
-const supabaseUrl = process.env.VITE_SUPABASE_URL;
-const supabaseKey = process.env.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl = "https://iymuizxrloixssfaueyp.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml5bXVpenhybG9peHNzZmF1ZXlwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTc1OTczMjEsImV4cCI6MjA3MzE3MzMyMX0.Ky_SoQbkGwbsEto7GSsVVJMQUJ7gl9hb60vevdsU8w8";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
