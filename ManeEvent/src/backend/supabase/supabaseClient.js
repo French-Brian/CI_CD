7471bc6ce1d67cc01c0b74a861d9730157913e8a
Brian French <french30@email.franklin.edu>
2025-10-25 12:30:05 -0400

    update supabaseclient

--- ManeEvent/src/backend/supabase/supabaseClient.js
+++ ManeEvent/src/backend/supabase/supabaseClient.js
@@ -5,8 +5,9 @@ import { createClient } from "@supabase/supabase-js";
 // they are in the .evn file in the root directory of the project
 // move this .env file to .gitignore to not expose keys  in production
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl = "https://iymuizxrloixssfaueyp.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml5bXVpenhybG9peHNzZmF1ZXlwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTc1OTczMjEsImV4cCI6MjA3MzE3MzMyMX0.Ky_SoQbkGwbsEto7GSsVVJMQUJ7gl9hb60vevdsU8w8";
 
 //crate a single supabase client
 const Supabase = createClient(supabaseUrl, supabaseKey);
