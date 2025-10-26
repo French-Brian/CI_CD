3fae4e1f172e217403c8c286aa43c49a07364077
Brian French <french30@email.franklin.edu>
2025-10-26 10:21:31 -0400

    update config

--- ManeEvent/vite.config.js
+++ ManeEvent/vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/French-Brian/CI_CD/",
+  base: "/CI_CD/",
 });
